88b2850c5ba45ee41d2cc10a3002f82f4034d603
Anthony Cole <anthony.cole83@yahoo.com>
2023-02-09 19:31:16 -0600

    completed problem 2 function.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,27 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+//Update the `cart.js` file to do the following:
+
+let basket = [];
+
+function addItem(item){
+    console.log('added item', item);
+        basket.push( item);
+return true;
+}
+console.log(addItem('bread'));
+console.log( addItem('milk'));
+console.log( addItem('cereal'));
+// Function works in the console
+
+function listItems(){
+    console.log('In listItems function');
+for(i=0; i < basket.length; i++){
+    console.log(basket[ i ]);
+    
+}return 'List Complete';
+}
+
+console.log(listItems());
+// Function woroks in console.
